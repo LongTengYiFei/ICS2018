0da6c422fe48a03f4646da48f7be10da64b331b5
tracer-ics2018 <tracer@njuics.org>
2020-01-15 14:59:32 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-26-generic #28-Ubuntu SMP Wed Dec 18 05:37:46 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  14:59:32 up  1:48,  1 user,  load average: 0.00, 0.02, 0.04 90c52c26a956f5d7ff353fba8fa75ae56cb28867

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -116,7 +116,7 @@ void ui_mainloop(int is_batch_mode) {
       args = NULL;
     }
 
-   // printf("%s\n",args);
+    printf("args is %s\n",args);
 #ifdef HAS_IOE
     extern void sdl_clear_event_queue(void);
     sdl_clear_event_queue();
