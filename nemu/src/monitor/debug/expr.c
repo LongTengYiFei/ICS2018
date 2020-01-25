6ae5f21879874c169af8104f68e2514bad31c31e
tracer-ics2018 <tracer@njuics.org>
2020-01-25 17:52:09 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-26-generic #28-Ubuntu SMP Wed Dec 18 05:37:46 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  17:52:09 up 47 min,  1 user,  load average: 0.13, 0.28, 0.17 77d27b06f9d5f333d2a827d4b0b93842a2ca5863

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -44,13 +44,12 @@ void init_regex() {
 
   for (i = 0; i < NR_REGEX; i ++) {
     ret = regcomp(&re[i], rules[i].regex, REG_EXTENDED);
+    printf("%s\n",re[i]);
     if (ret != 0) {
       regerror(ret, &re[i], error_msg, 128);
       panic("regex compilation failed: %s\n%s", error_msg, rules[i].regex);
     }
   }
- // for(int j = 0; j <= NR_REGEX-1;j++)
-   //  printf("%s\n",re[j]);
   
 }
 
