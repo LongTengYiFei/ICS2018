a17773b94ded1780fac7895d5aa86bf40670ff37
tracer-ics2018 <tracer@njuics.org>
2020-01-25 13:52:52 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-26-generic #28-Ubuntu SMP Wed Dec 18 05:37:46 UTC 2019 x86_64 x86_64 x86_64 GNU/Linux  13:52:52 up 30 min,  1 user,  load average: 0.04, 0.10, 0.16 5642defda0dea7fdf6044099f12f3d6974cf0b81

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -78,8 +78,13 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-	printf("%s\n", rules[i].regex);
-        strcpy(tokens[nr_token].str,rules[i].regex);
+	if(strcmp(rules[i].regex,"==") == 0)
+        strcpy(tokens[nr_token].str,"==");
+
+	if(strcmp(rules[i].regex,"\\+") == 0)
+        strcpy(tokens[nr_token].str,"+");
+        
+	nr_token ++;
         switch (rules[i].token_type) {
           default: ;
         }
