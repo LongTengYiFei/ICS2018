7a1117e11d86a8d29879923f9b61583ac76d914f
tracer-ics2018 <tracer@njuics.org>
2020-01-30 17:06:12 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:06:12 up  2:55,  1 user,  load average: 0.03, 0.03, 0.00 37419e19582dee0adc699c7d2b8bb53e8ded823f

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -35,6 +35,7 @@ WP* new_wp(char * express){
 	
 	wp_pool[i].expr = (char *)malloc((strlen(express)+1) * sizeof(char));
 	strcpy(wp_pool[i].expr,express);
+	wp_pool[i].expr[strlen(express)] = '\0';
 	printf("wp_pool[%d].expr is :%s\n",i,wp_pool[i].expr);
         nr_free_wp --;
 	printf("The new wp is NO.%d\n",i+1);
