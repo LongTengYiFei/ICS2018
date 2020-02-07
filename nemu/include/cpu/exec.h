ef3b9353f33567f557e54866c8f8364e96b446d1
tracer-ics2018 <tracer@njuics.org>
2020-02-07 14:38:42 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:38:42 up  3:53,  1 user,  load average: 0.40, 0.14, 0.03 6e5ff6f69ad46799a92fc3a53868d19c5b1a95ad

--- nemu/include/cpu/exec.h
+++ nemu/include/cpu/exec.h
@@ -10,7 +10,6 @@ typedef void (*EHelper) (vaddr_t *);
 
 static inline uint32_t instr_fetch(vaddr_t *eip, int len) {
   uint32_t instr = vaddr_read(*eip, len);
-  printf("Instr_fetch! Get instr 0x%x\n",instr);
 #ifdef DEBUG
   uint8_t *p_instr = (void *)&instr;
   int i;
