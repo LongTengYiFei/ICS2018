402ba46d1302fc81cd5fd0259011f3d99fae97a1
tracer-ics2018 <tracer@njuics.org>
2020-02-10 11:43:27 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:43:27 up  2:52,  1 user,  load average: 0.00, 0.02, 0.00 8ffb2951dfe85134aa818d2624da9ebdcce75435

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -1,8 +1,7 @@
 #include "cpu/exec.h"
 
 make_EHelper(mov) {
-  printf("EHelper mov :") ;
-  printf("seq_eip=0x%x\n", decoding.seq_eip);
+
   operand_write(id_dest, &id_src->val);
   print_asm_template2(mov);
 }
