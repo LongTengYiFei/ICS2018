52f031685f695bd38aeac1dfb50cc3ecca5b2fb4
tracer-ics2018 <tracer@njuics.org>
2020-02-13 09:44:49 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:44:49 up 21 min,  1 user,  load average: 0.17, 0.16, 0.11 f17acd5160ee0ffd12e7267f6ab82f90322f9938

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -7,23 +7,23 @@ make_EHelper(add) {
 }
 
 make_EHelper(sub) {
-  printf("EHelper(sub) begin!\n");
+  //printf("EHelper(sub) begin!\n");
 
-  printf("id_dest->val=0x%x ",id_dest->val);
-  printf("id_src->val=0x%x ",id_src->val);
-  printf("id_dest->width=0x%x ",id_dest->width);
-  printf("id_src->width=0x%x\n",id_src->width);
+  //printf("id_dest->val=0x%x ",id_dest->val);
+  //printf("id_src->val=0x%x ",id_src->val);
+  //printf("id_dest->width=0x%x ",id_dest->width);
+  //printf("id_src->width=0x%x\n",id_src->width);
   
   rtl_sext(&t1, &id_dest->val, id_dest->width);
-  printf("sizeof(t1)=%ld ",sizeof(t1));
+  //printf("sizeof(t1)=%ld ",sizeof(t1));
   rtl_sext(&t2, &id_src->val, id_src->width);
-  printf("sizeof(t2)=%ld \n",sizeof(t2));
-  printf("EHelper(sub) over!\n");
+  //printf("sizeof(t2)=%ld \n",sizeof(t2));
+  //printf("EHelper(sub) over!\n");
   //t0 result, t1 reg, t2 imm
   rtl_sub(&t0, &t1, &t2);
-  printf("t0 = 0x%x ",t0);
-  printf("t1 = 0x%x ",t1);
-  printf("t2 = 0x%x\n",t2);
+  //printf("t0 = 0x%x ",t0);
+  //printf("t1 = 0x%x ",t1);
+  //printf("t2 = 0x%x\n",t2);
 
   //set flags of sf zf af pf cf
   //CF
@@ -37,7 +37,7 @@ make_EHelper(sub) {
   rtl_update_ZFSF(&t0, 4);
   //write back
   operand_write(id_dest, &t0);
-  printf("EHelper(sub) over!\n");
+  //printf("EHelper(sub) over!\n");
   print_asm_template2(sub);
 }
 
