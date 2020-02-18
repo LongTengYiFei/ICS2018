cb2964fbd79e4d01d32ef8e60e979594ec9d37b1
tracer-ics2018 <tracer@njuics.org>
2020-02-18 17:21:37 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:21:37 up  9:01,  1 user,  load average: 0.09, 0.07, 0.01 deee223834d3e0c4636df25650abbf7858a7981

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -16,24 +16,22 @@ make_EHelper(test) {
 }
 
 make_EHelper(and) {
-  //printf("begin and!\n");
+  printf("begin and!\n");
   //cf =0,sf =0
   t0 = 0;
   rtl_set_CF(&t0);
   rtl_set_OF(&t0);
 
-  //printf("id_dest->val = 0x%x ",id_dest->val);
-  //printf("id_src->val = 0x%x\n",id_src->val);
-  //printf("cpu.esp = 0x%x\n",cpu.esp);
+  printf("id_dest->val = 0x%x ",id_dest->val);
+  printf("id_src->val = 0x%x\n",id_src->val);
   //opration and
   rtl_and(&t1, &id_dest->val, &id_src->val);
-  //printf("t1 = 0x%x\n",t1);
+  printf("t1 = 0x%x\n",t1);
   //zf sf  
   rtl_update_ZFSF(&t1, id_dest->width);
   //write back
   operand_write(id_dest, &t1);
-  //printf("write back ,cpu.esp  = 0x%x\n",cpu.esp);
-  //printf("and over!\n");
+  printf("and over!\n");
 
   print_asm_template2(and);
 }
