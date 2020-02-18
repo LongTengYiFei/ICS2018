1676031b6da9fb995ed624952d0bb034b248e100
tracer-ics2018 <tracer@njuics.org>
2020-02-18 20:09:35 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:09:35 up 11:49,  1 user,  load average: 0.06, 0.07, 0.05 d00ecc9f244d4e867c6dd6ae8a208e45b938199a

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -218,7 +218,7 @@ static make_EHelper(2byte_esc) {
 make_EHelper(real) {//exec_real
   //printf("Come in exec_real! ");
   uint32_t opcode = instr_fetch(eip, 1);
-  printf("eip = 0x%x ",eip);
+  printf("*eip = 0x%x ",*eip);
   printf("Get opcode 0x%x\n",opcode);
   decoding.opcode = opcode;
   set_width(opcode_table[opcode].width);
