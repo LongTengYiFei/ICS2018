7fb81fbd0deb260652e402793a14efc4d260c1f5
tracer-ics2018 <tracer@njuics.org>
2020-02-18 18:09:11 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-29-generic #31-Ubuntu SMP Fri Jan 17 17:27:26 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:09:11 up  9:48,  1 user,  load average: 0.18, 0.19, 0.11 b57f7f6c4b453c4694a84f244d4846b910590250

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -182,6 +182,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
   // M[esp] <- src1
   cpu.esp -= 4;
   vaddr_write(cpu.esp, *src1, 4);
+  printf("rtl push, *src1 = 0x%x\n",*src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
