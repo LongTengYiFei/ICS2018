441edb34b5e5d13063c13a76b7f80b4cd9a1a9e6
tracer-ics2018 <tracer@njuics.org>
2020-02-21 12:48:04 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:48:04 up  2:41,  1 user,  load average: 0.03, 0.04, 0.00 aedcd4f0c0f0fb32e48a265429193b252bfdea13

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -73,14 +73,14 @@ void difftest_step(uint32_t eip) {
   // TODO: Check the registers state with the reference design.
   // Set `nemu_state` to `NEMU_ABORT` if they are not the same.
   //
-  printf("ref_r.eax = 0x%x\n",ref_r.eax);
-  printf("ref_r.ecx = 0x%x\n",ref_r.ecx);
-  printf("ref_r.edx = 0x%x\n",ref_r.edx);
-  printf("ref_r.ebx = 0x%x\n",ref_r.ebx);
-  printf("ref_r.esp = 0x%x\n",ref_r.esp);
-  printf("ref_r.ebp = 0x%x\n",ref_r.ebp);
-  printf("ref_r.esi = 0x%x\n",ref_r.esi);
-  printf("ref_r.edi = 0x%x\n",ref_r.edi);
-  printf("ref_r.eip = 0x%x\n",ref_r.eip);
+  printf("cpu.eax = 0x%x ,ref_r.eax = 0x%x\n",cpu.eax, ref_r.eax);
+  printf("cpu.ecx = 0x%x ,ref_r.ecx = 0x%x\n",cpu.ecx, ref_r.ecx);
+  printf("cpu.edx = 0x%x ,ref_r.edx = 0x%x\n",cpu.edx, ref_r.edx);
+  printf("cpu.ebx = 0x%x ,ref_r.ebx = 0x%x\n",cpu.ebx, ref_r.ebx);
+  printf("cpu.esp = 0x%x ,ref_r.esp = 0x%x\n",cpu.esp, ref_r.esp);
+  printf("cpu.ebp = 0x%x ,ref_r.ebp = 0x%x\n",cpu.ebp, ref_r.ebp);
+  printf("cpu.esi = 0x%x ,ref_r.esi = 0x%x\n",cpu.esi, ref_r.esi);
+  printf("cpu.edi = 0x%x ,ref_r.edi = 0x%x\n",cpu.edi, ref_r.edi);
+  printf("cpu.eip = 0x%x ,ref_r.eip = 0x%x\n",cpu.eip, ref_r.eip);
   TODO();
 }
