9682d18ec9d3df74097998cfc6115be1b3364ec6
tracer-ics2018 <tracer@njuics.org>
2020-02-22 09:35:39 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:35:39 up  1:05,  1 user,  load average: 0.00, 0.01, 0.00 fb25a0866c5aabfeef1bb4502a3d51f44a26f34a

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -25,6 +25,7 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
     case CC_S:
     case CC_L:
     case CC_LE:
+      *dest = ((cpu.eflags.ZF) || (cpu.eflags.SF != cpu.eflags.OF));
       TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
