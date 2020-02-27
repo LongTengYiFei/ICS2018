b4a2b65140f9b60f545b42ab81cc27991d58fcfc
tracer-ics2018 <tracer@njuics.org>
2020-02-27 15:06:30 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:06:30 up  5:50,  1 user,  load average: 0.05, 0.14, 0.12 89809975fd800eb0f458a883cca32e79b0895980

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,13 +45,14 @@ make_EHelper(in) {
   //TODO();
   print_asm_template2(in);
 /*
-  printf("id_dest->width is 0x%x\n",id_dest->width);
   printf("id_src->val is 0x%x\n",id_src->val);
   printf("id_dest->val is 0x%x\n",id_dest->val);
   printf("dx is 0x%x\n",cpu.edx & 0x0000ffff);
   printf("eax is 0x%x\n",cpu.eax);
 */
   
+  printf("in id_dest->width is 0x%x\n",id_dest->width);
+  printf("in id_src->width is 0x%x\n",id_src->width);
   if(id_src->width == 1)
     reg_b(R_AL) = pio_read_b(id_src->val);
   else if(id_src->width == 2)
@@ -74,14 +75,14 @@ make_EHelper(out) {
   printf("out dest value is 0x%x\n",id_dest->val);
   printf("port dx is 0x%x\n",cpu.edx & 0x0000ffff);
   printf("eax is 0x%x\n",cpu.eax);*/
-/*
+
   if(id_src->width == 1)
     pio_write_b(id_dest->val,id_src->val);
   else if(id_src->width == 2)
     pio_write_w(id_dest->val,id_src->val);
   else if(id_src->width == 4)
     pio_write_l(id_dest->val,id_src->val);
-  */
+
 #if defined(DIFF_TEST)
   difftest_skip_ref();
 #endif
