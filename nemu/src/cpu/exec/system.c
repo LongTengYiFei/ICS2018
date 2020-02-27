16b76f3ca87e39da4b2e4ce7b4831aab0eb5cc00
tracer-ics2018 <tracer@njuics.org>
2020-02-27 11:23:56 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:23:56 up  2:08,  1 user,  load average: 0.01, 0.02, 0.00 b15737f4efd88d09c291e170a44523a938b9bb47

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -45,7 +45,7 @@ make_EHelper(in) {
   //printf("in begin!\n");
   //TODO();
   print_asm_template2(in);
-  int read = pio_read_b(id_dest->val);
+  int read = pio_read_b(id_dest->reg);
   printf("read is 0x%x\n",read);
   printf("dl is 0x%x\n",cpu.edx & 0x000000ff);
 
