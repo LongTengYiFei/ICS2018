23adf4ded87b55ae802772d2b49e2254f2b2da4e
tracer-ics2018 <tracer@njuics.org>
2020-02-29 17:03:17 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:03:17 up  7:57,  1 user,  load average: 0.40, 0.12, 0.03 4ad5b1d735604bbf30c817cc0b9adf788fd08d8

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -9,8 +9,8 @@ size_t video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _VideoInfoReg *info = (_VideoInfoReg *)buf;
-      info->width = 50;
-      info->height = 50;
+      info->width = 0;
+      info->height = 0;
       return sizeof(_VideoInfoReg);
     }
   }
