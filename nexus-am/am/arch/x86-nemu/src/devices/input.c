3effb7a70d07d04be551ce24f1e2d3253172d4ea
tracer-ics2018 <tracer@njuics.org>
2020-02-29 14:54:46 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:54:46 up  5:49,  1 user,  load average: 0.17, 0.16, 0.07 5dc98eb2c9a93ad8f745109abde5e710c990411d

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -12,7 +12,7 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
       uint32_t p = inl(0x60);
-      printf("p  = %d\n",p);
+      //printf("p  = %d\n",p);
       kbd->keydown = ((p & 0x8000) == 0) ? 0: 1;
       kbd->keycode = p;
       return sizeof(_KbdReg);
