cbb27f57e4938b5e602603ccedfd5c3e1dba6239
tracer-ics2018 <tracer@njuics.org>
2020-02-29 11:32:02 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:32:02 up  2:26,  1 user,  load average: 0.08, 0.04, 0.01 148023053c6a86dfd00c119058cf4860e81a2694

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -2,13 +2,19 @@
 #include <x86.h>
 #include <amdev.h>
 //0x60 port
+//breakcode is keycode  ---release
+//makecode is keycode | 0x8000  -----push
+//if push keydown is 1
+//if release keydown is 0
 size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
-      int code = inb(0x60);
-      kbd->keydown = code;
-      kbd->keycode = _KEY_NONE;
+      int code = inl(0x60);
+      if(code == _KEY_NONE)
+	      kbd->keydown = 0;
+      else kbd->keydown = 1;
+      kbd->keycode = code;
       return sizeof(_KbdReg);
     }
   }
