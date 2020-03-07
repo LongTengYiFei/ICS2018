cdcf282097941f0cde8816a494246fbcc7d9ef8a
tracer-ics2018 <tracer@njuics.org>
2020-03-07 17:24:49 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:49 up  5:50,  1 user,  load average: 0.05, 0.05, 0.01 b81a397c8559a922baa0d478e7e71369f3048dba

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -1,5 +1,6 @@
 #include <am.h>
 #include <x86.h>
+#include <stdio.h>
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 void vectrap();
@@ -9,6 +10,7 @@ _Context* irq_handle(_Context *tf) {
   _Context *next = tf;
   if (user_handler) {
     _Event ev = {0};
+    printf("0x%x\n",tf->irq);
     switch (tf->irq) {
       case 0x20: ev.event = _EVENT_IRQ_TIMER; break;
       case 0x80: ev.event = _EVENT_SYSCALL; break;
