d0867425c0cceb87249d1696755256d841073abc
tracer-ics2018 <tracer@njuics.org>
2020-03-10 11:09:12 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:09:12 up  1:19,  1 user,  load average: 0.00, 0.00, 0.00 4722e118ae79bfc7239d33964f5c862ec6166b8

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -10,13 +10,13 @@ typedef uint32_t size_t;
 
 struct _Context {
   //uintptr_t eflags, cs, eip;
-  //uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
+  uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
   //uintptr_t eax, ecx, edx, ebx, pre_esp, ebp, esi, edi;
   struct _Protect *prot;
-  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+  //uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int irq;
-  uintptr_t err;
-  uintptr_t eip, cs, eflags;
+  //uintptr_t err;
+  //uintptr_t eip, cs, eflags;
 };
 
 #define GPR1 eax
