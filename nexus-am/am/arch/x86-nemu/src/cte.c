328a73f7cd741998cecc24d5ce7d5a4e4e364a29
tracer-ics2018 <tracer@njuics.org>
2020-03-10 13:20:53 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:20:53 up  3:30,  1 user,  load average: 0.15, 0.06, 0.01 b3eb451d938c1c7dcf35b8e3dad5c29fe09c43d6

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -24,6 +24,7 @@ _Context* irq_handle(_Context *tf) {
     printf("tf->irq = 0x%x\n",tf->irq);
     printf("tf->irq = d %d\n",tf->irq);
     printf("tf->irq = c %c\n",tf->irq);
+    printf("tf->err = 0x%x\n",tf->err);
 
     printf("tf->eip = 0x%x\n",tf->eip);
     printf("tf-cs = 0x%x\n",tf->cs);
