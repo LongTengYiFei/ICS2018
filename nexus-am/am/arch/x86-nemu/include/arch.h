f6378683cde76cd8fabd286f0a0295be166f5a2d
tracer-ics2018 <tracer@njuics.org>
2020-03-10 10:27:27 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:27:27 up 37 min,  1 user,  load average: 0.06, 0.05, 0.05 13861c44c7926aeb8e651c47cbca4a7f6e95e2d4

--- nexus-am/am/arch/x86-nemu/include/arch.h
+++ nexus-am/am/arch/x86-nemu/include/arch.h
@@ -12,8 +12,8 @@ struct _Context {
   //uintptr_t eflags, cs, eip;
   //uintptr_t esi, ebx, eax, eip, edx, err, eflags, ecx, cs, esp, edi, ebp;
   //uintptr_t eax, ecx, edx, ebx, pre_esp, ebp, esi, edi;
-  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   struct _Protect *prot;
+  uintptr_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
   int irq;
   uintptr_t err;
   uintptr_t eip, cs, eflags;
