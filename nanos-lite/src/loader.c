5809fcacf21aa0860e51db8ea00f314572033e44
tracer-ics2018 <tracer@njuics.org>
2020-03-14 16:48:56 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:48:56 up  4:55,  1 user,  load average: 0.00, 0.00, 0.00 5b713ded317461fa76a7addd1b8978fa55c806e

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,7 +7,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   printf("ramdisk size = %d\n",get_ramdisk_size());
   char *buf = NULL;
   ramdisk_read(buf, 0, get_ramdisk_size());
-  printf("buf[0] = %c", buf[0]);
+  printf("buf[0] = %d\n", buf[0]);
+  printf("buf[1] = %d\n", buf[1]);
+  printf("buf[2] = %d\n", buf[2]);
   printf("loader over!\n");
   printf("\n");
   TODO();
