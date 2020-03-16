f42bcc2e54b5cb2ea606149c3bd83cb37595688f
tracer-ics2018 <tracer@njuics.org>
2020-03-16 15:27:19 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:27:19 up 22 min,  1 user,  load average: 0.33, 0.15, 0.11 1a177001644ab5ce9a69a742c8dd05b6356cb9cd

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -17,10 +17,90 @@ int printf(const char *fmt, ...) {
         else break;  
   return 0;
 }
+int write_Int(char *buffer, int value, int prefix_n, char prefix, int digit){
+   int len = 0;
+   char rec[100];
+   int vt = value;
+   if(value < 0){
+      value*= -1;
+      *buffer++ = '-';
+   }
+   while(1){
+    if(digit){
+      int t = value %10;
+      rec[len++] = '0'+t;
+      value /=10;
+    }else{
+	    int t = value % 16;
+	    rec[len] = t<10?'0'+t:'a'+t-10;
+	    value /=16;
+    }
+   if(!value)break;
+   }
+   int tmp = len;
+   if(len < prefix_n){
+	   int j = prefix_n -len;
+	      while(j -- ){
+		      *buffer++ = prefix;
+	      }
+	      len = prefix_n;
+   }
+   while(tmp -- )
+	   *buffer++ =rec[tmp];
 
+   return vt >=0?len:len+1;
+}
 int vsprintf(char *out, const char*fmt, va_list ap){
+ char *buffer = out;
+ unsigned int fmt_length = strlen(fmt);
+ unsigned int index = 0;
+ 
+ int temp;
+ char ctemp;
+ char *stemp;
+ int width = 0;
+ char apd = ' ';
 
- return 0;
+ for(index = 0;index<=fmt_length;++index){
+	 if(fmt[index]!='%')
+		 (*buffer++)=fmt[index];
+	 else{
+		 index++;
+		 while(fmt[index] == ' '||fmt[index] == '0'){
+			 if(fmt[index] == ' ')apd = ' ';
+			 else if(fmt[index] == '0')apd = '0';                         
+			 index++;
+		 }
+		 width = 0;
+		 if(fmt[index] >='0' &&fmt[index] <='9'){
+			 while(fmt[index]>='0' &&fmt[index]<='9'){width = width *10+fmt[index] - '0';
+				 index++;
+			 }
+		 }
+
+  switch(fmt[index]){
+	  case 'd':
+		 temp=va_arg(ap,int);
+		buffer=buffer+write_Int(buffer,temp,width,apd,1);
+	       break;
+	  case's':
+stemp=(char*)va_arg(ap,char*);
+strcpy(buffer,stemp);
+buffer+=strlen(stemp);
+break;
+	  case'c':
+ctemp=va_arg(ap,int);
+*buffer++ = ctemp;
+break;
+	  case'x':
+temp=va_arg(ap,int);
+buffer=buffer+write_Int(buffer,temp,width,apd,0);
+break;
+	  case'p':
+temp=va_arg(ap,int);
+buffer=buffer+write_Int(buffer,temp,width,apd,0);
+  }}}
+ return strlen(out);
 }
 int sprintf(char *out, const char *fmt, ...) {
   va_list args;
