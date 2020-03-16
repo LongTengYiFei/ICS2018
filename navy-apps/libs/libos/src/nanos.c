2959c6435f7b9b7da82d980233202b43cc08132f
tracer-ics2018 <tracer@njuics.org>
2020-03-16 15:41:03 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-40-generic #32-Ubuntu SMP Fri Jan 31 20:24:34 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:41:03 up 35 min,  1 user,  load average: 0.04, 0.03, 0.04 dbb031fcb3d549de154234be4e726a176721af53

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -8,6 +8,7 @@
 
 #if defined(__ISA_X86__)
 intptr_t _syscall_(int type, intptr_t a0, intptr_t a1, intptr_t a2){
+  printf("come into syscall\n");
   int ret = -1;
   asm volatile("int $0x80": "=a"(ret): "a"(type), "b"(a0), "c"(a1), "d"(a2));
   return ret;
