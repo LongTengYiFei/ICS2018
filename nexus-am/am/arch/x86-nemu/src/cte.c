270dfba8d85383161338c74a82d61197a0542ea2
tracer-ics2018 <tracer@njuics.org>
2020-03-18 14:11:08 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:11:08 up  2:28,  1 user,  load average: 0.18, 0.08, 0.02 d9de276ed42994dcdc25cdf6576e2b9652d223b

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -41,9 +41,9 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_KERN);
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
-  printf("idt has been inited\n\n");
+  //printf("idt has been inited\n\n");
   set_idt(idt, sizeof(idt));
-
+  printf("set_idt over\n\n");
   // register event handler
   user_handler = handler;
 
