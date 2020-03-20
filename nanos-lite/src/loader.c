4242e09857f06795c7ac9751293917d51d3abe6d
tracer-ics2018 <tracer@njuics.org>
2020-03-20 15:56:44 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:56:44 up 22 min,  1 user,  load average: 0.00, 0.06, 0.08 8d413aa1f15ea7ac7ffaf659f9d51116c46801a

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,20 +3,13 @@
 #define DEFAULT_ENTRY 0x4000000
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  //printf("come into loader!\n");
-  //printf("ramdisk size = %d\n",get_ramdisk_size());
   int ramdisk_size = get_ramdisk_size();
-  ramdisk_read(DEFAULT_ENTRY, 0, ramdisk_size);
-  //printf("ramdisk_read over\n\n");	  
-  //printf("loader over!\n");
-  //printf("\n");
-  //TODO();
+  ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
   return DEFAULT_ENTRY;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  printf("entry = %d\n\n",entry);
   ((void(*)())entry) ();
 }
 
