7faa39d589aaca1eb0c2536566c5085bb6bd0c6a
tracer-ics2018 <tracer@njuics.org>
2020-03-21 11:54:48 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:54:48 up 57 min,  1 user,  load average: 0.12, 0.16, 0.11 8a26bbb978e473a13e0cf2ec7942068e7bc7eec3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -8,7 +8,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   return DEFAULT_ENTRY;
 }
 
-void naive_uload(PCB *pcb, const char *filename) {
+void naive_uload(PCB *pcb, const char *filename) { 
   uintptr_t entry = loader(pcb, filename);
   ((void(*)())entry) ();
 }
