774090f65ab4a53369a63fb3d952e00fb1442938
tracer-ics2018 <tracer@njuics.org>
2020-03-21 16:07:49 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:07:49 up  5:10,  1 user,  load average: 0.05, 0.05, 0.02 a269b37eb632a60f5f1c797bc3e0e6ef6e8815dd

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,7 +3,6 @@
 #include "proc.h"
 uintptr_t sys_yield();
 void sys_exit(int exit_code);
-
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -11,26 +10,32 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
-  
+ /* 
   printf("come into do syscall!\n");
   printf("a[0] = 0x%x\n",a[0]);
   printf("a[1] = 0x%x\n",a[1]);
   printf("a[2] = 0x%x\n",a[2]);
   printf("a[3] = 0x%x\n",a[3]);
   printf("\n");
-  
+  */
   switch (a[0]) {
     case SYS_exit: sys_exit(a[0]); break;
     case SYS_yield: c->GPR1 = sys_yield();break;
+    case SYS_write: printf("SYS_write!\n"); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
 }
+
 uintptr_t sys_yield(){
   _yield();
   return 0;
 }
 void sys_exit(int exit_code){
   _halt(exit_code);
+}
+uintptr_t sys_write(){
+
+   return 0;
 }
