133b9be003ba9161108d35eb667a6e8fcec5982f
tracer-ics2018 <tracer@njuics.org>
2020-03-23 17:19:24 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:19:24 up  3:29,  1 user,  load average: 0.39, 0.13, 0.04 a12d2846569c942a6ad9b36a93d536b1c0af34a

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,7 +12,6 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-	printf("addr %x\n",addr);
   if(is_mmio(addr) == -1)
     return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
   else return mmio_read(addr, len, is_mmio(addr));
