bb49f1b8352f0e8f2ba00c2402b39de97707a1ef
tracer-ics2018 <tracer@njuics.org>
2020-03-23 17:43:41 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:43:41 up  3:53,  1 user,  load average: 0.11, 0.05, 0.01 117b5170ddb8dc65c877f37e93ffc442b6fb4dc

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -7,9 +7,10 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
   //we can ignore the flags and mode
   int fd = fs_open(filename, 0, 0);
   printf("fd = %d\n",fd);
-  assert(0);
-  //fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));//all of this file
+  size_t len = fs_read(fd, (void*)DEFAULT_ENTRY, fs_filesz(fd));//all of this file
+  printf("len = %d\n",len);
   //fs_close(fd);
+  assert(0);
   return entry;
 }
 
