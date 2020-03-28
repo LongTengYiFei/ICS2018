b573a3e9a90ebb4e6774a30e1ddd5691a122387f
tracer-ics2018 <tracer@njuics.org>
2020-03-28 16:04:36 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:04:36 up  5:11,  1 user,  load average: 0.00, 0.03, 0.04 b8948cd95f38014ec3dff6ec6baa718324f45cbb

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -3,8 +3,9 @@
 
 int main() {
   FILE *fp = fopen("/share/texts/num", "r+");
+  printf("fp = 0x%x\n",fp);
+  printf("fp = %d\n",fp);
   assert(fp);
-  printf("text open over\n");
   fseek(fp, 0, SEEK_END);
   long size = ftell(fp);
   assert(size == 5000);
