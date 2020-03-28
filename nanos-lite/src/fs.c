131ef96327ad5a57e0ed3e3d6430a68cccd1fbf8
tracer-ics2018 <tracer@njuics.org>
2020-03-28 12:19:23 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:19:23 up  1:26,  1 user,  load average: 0.18, 0.07, 0.01 1481cabb84de5f4ba8b6744981cee015f46fcba1

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -26,12 +26,12 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  {"stdin", 0, 0, 0, invalid_read, serial_write},
+  {"stdin", 0, 0, 0, invalid_read, invalid_write},
   {"stdout", 0, 0, 0, invalid_read, serial_write},
-  {"stderr", 0, 0, 0, invalid_read, },
+  {"stderr", 0, 0, 0, invalid_read, serial_write},
   [FD_FB] = {"/dev/fb", 0, 0, 0, NULL, fb_write},
   [FD_EVENTS] = {"/dev/events", 0, 0, 0, events_read},
-  [FD_DISPINFO] = {"/proc/dispinfo", 129 ,0, 0, dispinfo_read},
+  [FD_DISPINFO] = {"/proc/dispinfo", 128 ,0, 0, dispinfo_read},
   [FD_TTY] = {"/dev/tty", 0, 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
