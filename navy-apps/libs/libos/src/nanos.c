0f7c537f16b6ac625f59356712af822c43f6a0e9
tracer-ics2018 <tracer@njuics.org>
2020-03-31 14:58:40 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-42-generic #34-Ubuntu SMP Fri Feb 28 05:49:40 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:58:40 up  7:03,  1 user,  load average: 0.26, 0.14, 0.06 4c03d113c956cfec91a4ecc73b794ce6cda2923e

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -37,12 +37,14 @@ int _write(int fd, void *buf, size_t count){
 }
 
 extern char _end;
-intptr_t program_break_old = (intptr_t)&_end;
+intptr_t program_break = (intptr_t)&_end;
 
 void *_sbrk(intptr_t increment){
-  intptr_t program_break_new = program_break_old + increment;
-  if(_syscall_(SYS_brk, program_break_new, 0, 0) == 0)
-	  return (void *) program_break_old;
+  intptr_t old_program_break = program_break;
+  if(_syscall_(SYS_brk, old_program_break + increment, 0, 0) == 0){
+	  program_break += increment;
+	  return (void *) old_program_break;
+  }
   else
           return (void *)-1;
 }
