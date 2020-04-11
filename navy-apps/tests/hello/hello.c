2e0fd87cce99712d12c1a06864bc997a5638ad84
tracer-ics2018 <tracer@njuics.org>
2020-04-11 16:20:34 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:20:34 up  4:51,  1 user,  load average: 0.08, 0.12, 0.12 896dca2a449e6608ad8f8fa580844a22ea069bb

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 int main() {
-  write(1, "Hello World!\n", 13);
+  //write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
   while (i<=100) {
