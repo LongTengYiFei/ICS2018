0b57a85b44cf888633ab35236a8421af8e530315
tracer-ics2018 <tracer@njuics.org>
2020-04-11 18:19:09 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:19:09 up  6:49,  1 user,  load average: 0.23, 0.68, 0.59 f31e935e7fb2fff7d5b5013d48cc85215b3ad9e0

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -50,7 +50,6 @@ void init_difftest(char *ref_so_file, long img_size) {
   ref_difftest_memcpy_from_dut(ENTRY_START, guest_to_host(ENTRY_START), img_size);
   ref_difftest_setregs(&cpu);
 }
-extern int foot=0;
 void difftest_step(uint32_t eip) {
   //printf("DiffTest_step Come in!!\n");
   CPU_state ref_r;
@@ -72,13 +71,10 @@ void difftest_step(uint32_t eip) {
 
   //printf("ref_r.eflags.OF = 0x%x\n",ref_r.eflags.OF);
   //printf("ref_r.eflags.SF = 0x%x\n",ref_r.eflags.SF);
-  foot ++;
   // TODO: Check the registers state with the reference design.
   // Set `nemu_state` to `NEMU_ABORT` if they are not the same.
- if(cpu.eip >= 0x400000)//come into user program
  if(cpu.eax != ref_r.eax ||cpu.ecx != ref_r.ecx ||cpu.edx != ref_r.edx ||cpu.ebx != ref_r.ebx ||cpu.esp != ref_r.esp ||cpu.ebp != ref_r.ebp ||cpu.esi != ref_r.esi ||cpu.edi != ref_r.edi ||cpu.eip != ref_r.eip ){
   nemu_state = NEMU_ABORT;
-  printf("foot = %d\n",foot);
   printf("cpu.eax = 0x%x ,ref_r.eax = 0x%x\n",cpu.eax, ref_r.eax);
   printf("cpu.ecx = 0x%x ,ref_r.ecx = 0x%x\n",cpu.ecx, ref_r.ecx);
   printf("cpu.edx = 0x%x ,ref_r.edx = 0x%x\n",cpu.edx, ref_r.edx);
