47047f0d35c2cbe4c190b1e9538a53c1fb76b3d4
tracer-ics2018 <tracer@njuics.org>
2020-04-12 18:21:00 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:21:00 up  5:36,  1 user,  load average: 0.30, 0.11, 0.03 1492a31392fa39fb9df542cca57c7fb791a6d4c5

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -33,7 +33,6 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count){
-  putc("w");
   return _syscall_(SYS_write, fd, (uintptr_t)buf, count);
 }
 
