828595de86e93375b6e02ee58961cf57d460fe2c
tracer-ics2018 <tracer@njuics.org>
2020-04-13 18:20:29 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:20:29 up  4:41,  1 user,  load average: 0.13, 0.03, 0.01 7134d571766163e34d5a0ebe33a45f66ef6a9db

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -23,7 +23,7 @@ void difftest_on() {
         ref_difftest_memcpy_from_dut(0, guest_to_host(0), 0x7c00);
         ref_difftest_memcpy_from_dut(ENTRY_START, guest_to_host(ENTRY_START), PMEM_SIZE - ENTRY_START);
         ref_difftest_setregs(&cpu);
-
+        return ;
 }//on is not skip
 
 void init_difftest(char *ref_so_file, long img_size) {
