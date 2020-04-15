647ad2009b179253e9d3ffc5133739ded09fb779
tracer-ics2018 <tracer@njuics.org>
2020-04-15 12:31:07 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:31:07 up  2:15,  1 user,  load average: 0.20, 0.10, 0.08 b77726921cf5a179d0aa24dc10c38a7b5960a82b

--- nemu/src/monitor/diff-test/ref.c
+++ nemu/src/monitor/diff-test/ref.c
@@ -12,7 +12,8 @@ void difftest_getregs(void *r) {
 }
 
 void difftest_setregs(const void *r) {
-  memcpy(&cpu, r, DIFFTEST_REG_SIZE);
+  memcpy(&cpu, r, DIFFTEST_REG_SIZE);//GPR and eip
+  printf("?");
 }
 
 void difftest_exec(uint64_t n) {
