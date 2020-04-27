de291a35d009106a197355ff6e797a005052eb7b
tracer-ics2018 <tracer@njuics.org>
2020-04-27 17:08:09 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:08:09 up  3:47,  1 user,  load average: 0.25, 0.20, 0.08 6c61ddb04701047ec61da585c44a8d46c78c2eb0

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -134,13 +134,12 @@ make_EHelper(shl) {
 
   rtl_update_PF(&t2);
   cpu.eflags.AF = 0;
-  /*
+  //CF
   t3 = id_dest->val;
   for(int i=1;i<=id_src->val;i++){
 	  cpu.eflags.CF = t3 & 0x80000000;
           t3 = t3 << 1; 
   }
-*/
 
   print_asm_template2(shl);
 }
