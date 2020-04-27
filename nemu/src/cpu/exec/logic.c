a92a22143ed8d5ae42ed3dcb3d477ceb8998e848
tracer-ics2018 <tracer@njuics.org>
2020-04-27 17:05:56 +0800

    >  compile 201973389 hust Linux hust-desktop 5.3.0-46-generic #38-Ubuntu SMP Fri Mar 27 17:37:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:05:56 up  3:45,  1 user,  load average: 0.16, 0.06, 0.01 bf0c7985fab690f883429ef8adc3acc211b44efe

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -132,6 +132,16 @@ make_EHelper(shl) {
   //printf("shl over\n");
   // unnecessary to update CF and OF in NEMU
 
+  rtl_update_PF(&t2);
+  cpu.eflags.AF = 0;
+  /*
+  t3 = id_dest->val;
+  for(int i=1;i<=id_src->val;i++){
+	  cpu.eflags.CF = t3 & 0x80000000;
+          t3 = t3 << 1; 
+  }
+*/
+
   print_asm_template2(shl);
 }
 
