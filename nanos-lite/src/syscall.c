54e636720b111e736061a6eaf7eebeb5fff69a4e
tracer-ics2018 <tracer@njuics.org>
2020-05-18 16:25:58 +0800

    >  run 201973389 hust Linux hust-desktop 5.3.0-51-generic #44-Ubuntu SMP Wed Apr 22 21:09:44 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:25:58 up  7:21,  1 user,  load average: 1.07, 1.07, 0.56 7e3fbc1b623e8cee186748c0a3239e372f43de4d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,6 +14,7 @@ _Context* do_syscall(_Context *c) {
   a[1] = c->GPR2;
   a[2] = c->GPR3;
   a[3] = c->GPR4;
+  Log("a[0] = %d",a[0]);
   switch (a[0]) {
     case SYS_exit: naive_uload("NULL", "/bin/init"); break; c->GPR1 = -1;
     case SYS_yield: c->GPR1 = sys_yield(); break;
